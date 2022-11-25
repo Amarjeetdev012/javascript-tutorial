08a0700a9f5cfbb7f9fb7b899483618ed07457fc
unknown <amarjeet@baruzotech.com>
2022-11-25 15:39:51 +0530

    javascript boolean

---
+++ javascript_boolean.js
@@ -0,0 +1,10 @@
+//! Summary
+// JavaScript boolean type has two literal values true and false.
+// Use the Boolean() function to cast a non-boolean values to a boolean value.
+// Some statements implicitly cast a non-boolean value into a boolean value.
+
+// Data Type	Values converted to true	Value Converted to false
+// string	Any non-empty string	“” (empty string)
+// number	Any Non-zero number	0, NaN
+// object	Any object	null
+// undefined	(not relevant)	undefined
