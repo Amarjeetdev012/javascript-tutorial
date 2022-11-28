17d34f9cedd2ab39a76a5a65a0fb6907c2117292
Amarjeetdev012 <117710031+Amarjeetdev012@users.noreply.github.com>
2022-11-28 15:38:14 +0530

    continue

---
+++ continue.js
@@ -0,0 +1,27 @@
+// Summary
+// Use the JavaScript continue statement to skip the current iteration of a loop and continue the next one.
+
+// Using the continue statement in a while loop
+// When using the continue statement in a while loop, it doesn’t terminate the execution of the loop entirely. Instead, it jumps back to the condition.
+
+// The following flowchart show hows the continue statement works in a while loop statement:
+
+
+// The following example uses the continue statement in a while loop to display the odd numbers from 1 to 10:
+
+// let i = 0;
+// while (i < 10) {
+//   i++;
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+// Code language: JavaScript (javascript)
+// Output:
+
+// 1
+// 3
+// 5
+// 7
+// 9
