ef4ca03d5a1f0fcc8b5f4db52e8f2bf5bcc4517c
Amarjeetdev012 <117710031+Amarjeetdev012@users.noreply.github.com>
2022-11-28 15:27:31 +0530

    switch case

---
+++ ternary_operators.js
@@ -0,0 +1,34 @@
+// in this tutorial, you will learn how to use the JavaScript ternary operator to make your code more concise. 
+
+// let age = 18;
+// let message;
+
+// if (age >= 16) {
+//   message = 'You can drive.';
+// } else {
+//   message = 'You cannot drive.';
+// }
+
+// console.log(message);
+
+// In this example, we show a message that a person can drive if the age is greater than or equal to 16. Alternatively, you can use a ternary operator instead of the if-else statement like this:
+
+// let age = 18;
+// let message;
+
+// age >= 16 ? (message = 'You can drive.') : (message = 'You cannot drive.');
+
+// console.log(message);
+// Code language: JavaScript (javascript)
+// Or you can use the ternary operator in an expression as follows:
+
+// let age = 18;
+// let message;
+
+// message = age >= 16 ? 'You can drive.' : 'You cannot drive.';
+
+// console.log(message);
+// Code language: JavaScript (javascript)
+// Here’s the syntax of the ternary operator:
+
+// condition ? expressionIfTrue : expressionIfFalse;
