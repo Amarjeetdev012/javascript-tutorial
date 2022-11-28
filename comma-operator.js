a9251519334bde99009c5f3c6d25d94887f7e95f
Amarjeetdev012 <117710031+Amarjeetdev012@users.noreply.github.com>
2022-11-28 16:07:11 +0530

    comma operator

---
+++ comma-operator.js
@@ -0,0 +1,14 @@
+// Introduction to the JavaScript comma operator
+// JavaScript uses a comma (,) to represent the comma operator. A comma operator takes two expressions, evaluates them from left to right, and returns the value of the right expression.
+
+// Here’s the syntax of the comma operator:
+
+// leftExpression, rightExpression
+// For example:
+
+// let result = (10, 10 + 20);
+// console.log(result);
+// Code language: JavaScript (javascript)
+// Output:
+
+// 30
