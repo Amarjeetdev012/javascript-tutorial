bb0418da45c221b3d4d97664839232b50663bf20
unknown <amarjeet@baruzotech.com>
2022-11-28 17:38:36 +0530

    objects and prototype

--- annoynumus-function.js
+++
@@ -1,3 +0,0 @@
-//! Summary
-// Anonymous functions are functions without names.
-// Anonymous functions can be used as an argument to other functions or as an immediately invoked function execution.
--- pass-by-value-and-reference.js
+++
@@ -1,5 +0,0 @@
-//* Any changes that you make to the arguments inside the function do not reflect the passing variables outside of the function. In other words, the changes made to the arguments are not reflected outside of the function.
-
-//! Summary
-// JavaScript passes all arguments to a function by values.
-// Function arguments are local variables in JavaScript.
