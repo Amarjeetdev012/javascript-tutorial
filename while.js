ac2fa3f84762405c528d289b07b27593955751e8
Amarjeetdev012 <117710031+Amarjeetdev012@users.noreply.github.com>
2022-11-28 15:40:58 +0530

    while loop

---
+++ while.js
@@ -0,0 +1,23 @@
+// JavaScript while loop example
+// The following example uses the while statement to output the odd numbers between 1 and 10 to the console:
+
+// let count = 1;
+// while (count < 10) {
+//     console.log(count);
+//     count +=2;
+// }
+// Code language: JavaScript (javascript)
+// Output:
+
+// 1
+// 3
+// 5
+// 7
+// 9
+// How the script works
+
+// First, declare and initialize the count variable to 1.
+// Second, execute the statement inside the loop if the count variable is less than 10. In each iteration, ouput the count to the console and increase the count by 2.
+// Third, after 5 iterations, the count is 11. Therefore, the condition count < 10 is false, the loop exits.
+// Summary
+// Use a while loop statement to create a loop that executes a block as long as a condition evaluates to true.
