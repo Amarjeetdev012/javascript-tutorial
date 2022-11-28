ec5afcba663f827d89b94bd8ee489152f3836284
unknown <amarjeet@baruzotech.com>
2022-11-28 12:31:14 +0530

    assignment opertor

---
+++ JavaScript_Assignment_Operators.js
@@ -0,0 +1,24 @@
+//* Introduction to JavaScript assignment operators
+// An assignment operator (=) assigns a value to a variable. 
+// let a = b;
+// let counter = 0;
+
+//TODO The following table illustrates assignment operators that are shorthand for another operator and the assignment:
+
+// Operator	Meaning	Description
+// a = b	a = b	Assigns the value of b to a.
+// a += b	a = a + b	Assigns the result of a plus b to a.
+// a -= b	a = a - b	Assigns the result of a minus b to a.
+// a *= b	a = a * b	Assigns the result of a times b to a.
+// a /= b	a = a / b	Assigns the result of a divided by b to a.
+// a %= b	a = a % b	Assigns the result of a modulo b to a.
+// a &=b	a = a & b	Assigns the result of a AND b to a.
+// a |=b	a = a | b	Assigns the result of a OR b to a.
+// a ^=b	a = a ^ b	Assigns the result of a XOR b to a.
+// a <<= b	a = a << b	Assigns the result of a shifted left by b to a.
+// a >>= b	a = a >> b	Assigns the result of a shifted right (sign preserved) by b to a.
+// a >>>= b	a = a >>> b	Assigns the result of a shifted right by b to a.
+
+//! Summary
+// Use the assignment operator (=) to assign a value to a variable.
+// Chain the assignment operators if you want to assign a single value to multiple variables.
