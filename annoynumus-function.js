d2f1f6c23a2ea061d01061b862511ab7f4f54002
unknown <amarjeet@baruzotech.com>
2022-11-28 16:38:57 +0530

    annnoynmus function

---
+++ annoynumus-function.js
@@ -0,0 +1,3 @@
+//! Summary
+// Anonymous functions are functions without names.
+// Anonymous functions can be used as an argument to other functions or as an immediately invoked function execution.
