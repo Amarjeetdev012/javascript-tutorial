c9f85183657190fb95967119993f365504f58c6e
unknown <amarjeet@baruzotech.com>
2022-11-29 10:04:38 +0530

    javacript object defining property

---
+++ JavaScript-Object-Properties.js
@@ -0,0 +1,5 @@
+//! Summary
+// JavaScript objects have two types of properties: data properties and accessor properties.
+// JavaScript uses internal attributes denoted by [[...]] to describe the characteristics of properties such as [[Configurable]], [[Enumerable]], [[Writable]], and  [[Value]], [[Get]], and [[Set]].
+// The method Object.getOwnPropertyDescriptor() return a property descriptor of a property in an object.
+// A property can be defined directly on an object or indirectly via the Object.defineProperty() or Object.defineProperties() methods. These methods can be used to change the attributes of a property.
