1a1a5f25695ef6da8e30c5d2560cde340a0d5c88
unknown <amarjeet@baruzotech.com>
2022-11-29 10:17:40 +0530

    obj has own property

---
+++ Own-Properties-of-an-Object.js
@@ -0,0 +1,20 @@
+// const person = {
+//     firstName: 'John',
+//     lastName: 'Doe'
+// };
+
+// console.log(person)
+
+// const employee = Object.create(person, {
+//     job: {
+//         value: 'JS Developer',
+//         enumerable: true
+//     }
+// });
+
+// console.log(employee)
+// console.log(person)
+
+//! Summary
+// A property that is directly defined on an object is an own property.
+// The obj.hasOwnProperty() method determines whether or not a property is own.
