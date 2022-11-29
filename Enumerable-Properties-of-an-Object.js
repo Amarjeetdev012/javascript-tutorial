6b2c21d6fb879173ad3b02fbb593dd4fc7fad557
unknown <amarjeet@baruzotech.com>
2022-11-29 10:12:28 +0530

    enunaable property in object

---
+++ Enumerable-Properties-of-an-Object.js
@@ -0,0 +1,3 @@
+//! Summary
+// A property is enumerable if it has the enumerable attribute sets to true. The obj.propertyIsEnumerable() determines whether or not a property is enumerable.
+// A property created via a simple assignment or a property initializer is enumerable.
