803f0a9409583e2e7e53623913dcc95848d84bc2
unknown <amarjeet@baruzotech.com>
2022-11-29 10:08:50 +0530

    for in loop for objects

---
+++ JavaScript-for…in-Loop.js
@@ -0,0 +1,3 @@
+//! Summary
+// The for...in loop iterates over the enumerable properties of an object. It also goes up to the prototype chain and enumerates inherited properties.
+// Avoid using for...in loop to iterate over elements of an array, especially when the index order is important.
