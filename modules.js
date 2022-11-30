2d726ace1d3bccf24b031d010a85d8bcd1913aef
unknown <amarjeet@baruzotech.com>
2022-11-30 14:22:53 +0530

    module top level await and dynamic import

---
+++ modules.js
@@ -0,0 +1,12 @@
+//! Summary
+// we can use javascript import dynamically
+// Use JavaScript import() to dynamically load a module. The import() returns a Promise that will be fulfilled once the module is loaded completely.
+// Use the async / await to handle the result of the import().
+// Use the Promise.all() method to load multiple modules at once.
+// Use the object destructuring to assign variables to the exporting objects of a module.
+// Use the default keyword to access the default export.
+
+
+//* for using top level await 
+//todo first we have to change the file name js to mjs and then after we can use top level await javascript ecma script module 
+// in this module we can use await without using async keyword
