f632cb05ecced7a5403062fe51de2024a516394a
unknown <amarjeet@baruzotech.com>
2022-11-30 11:07:17 +0530

    javacript promise finally method will run without woory about resolve and reject

---
+++ javascroipt-promise-finally.js
@@ -0,0 +1,3 @@
+//! Summary
+// The finally() method schedule a function to execute when the promise is settled, either fulfilled or rejected.
+// It’s good practice to place the code that cleans up the resources in the finally() method once the promise is settled, regardless of its outcome.
