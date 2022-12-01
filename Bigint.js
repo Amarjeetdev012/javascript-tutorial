c7ed0869294a184c0a7e4bd459b8036c369fa541
unknown <amarjeet@baruzotech.com>
2022-12-01 12:44:05 +0530

    bigint

---
+++ Bigint.js
@@ -0,0 +1,3 @@
+//! Summary
+// The BigInt is a new primitive type that can represent whole numbers bigger than 253 - 1, which is the largest number Javascript can reliably represent with the number type.
+// Append n to a literal integer or use BigInt() function to create a bigint.
