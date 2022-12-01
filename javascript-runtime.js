a5580f9e7e6889f708d3b7538ed3229e5f14d3d6
unknown <amarjeet@baruzotech.com>
2022-12-01 11:21:59 +0530

    javascript runtime hoisting

---
+++ javascript-runtime.js
@@ -0,0 +1,4 @@
+//! Summary
+// JavaScript hoisting occurs during the creation phase of the execution context that moves the variable and function declarations to the top of the script.
+// The JavaScript engine hoists the variables declared using the let keyword, but it doesn’t initialize them as the variables declared with the var keyword.
+// The JavaScript engine doesn’t hoist the function expressions and arrow functions.
